74eac1b5411f5ed82ca52b44788c50c66a5d37fc
Marcus Heine <mheine@kth.se>
2018-03-13 15:21:26 +0100

    Refactor JS functions

--- js/content.js
+++ js/content.js
@@ -1,5 +1,3 @@
-var globalToggle = 0;
-
 $(function() {
 	$.getJSON( "metadata.json", function( itemdata ) {
 
@@ -73,13 +71,8 @@ function createHiddenDiaryImages(data) {
 				image.attr('id', subgroup[j].id + "_" + k);
 				image.attr('src', path);
 				$('#pre-load-images').append(image);
-
 			}
-
 		}
-
-
-
 	}
 }
 
@@ -96,8 +89,6 @@ function exportImage() {
 		image.css('width', "100%");
 		image.css('height', "100%");
 		$('#download-div').append(image);
-		
-		//ReImg.fromCanvas(canvas).downloadPng("osrs-progress")
 	});
 }
 
@@ -116,8 +107,6 @@ function createDividers() {
 	$("#div-pvm").append(line);
 
 	$('#div-gwd').prepend(gwdLine);
-	//$("#div-gwd").prepend(line);
-
 }
 
 function createSection(data, divID) {
@@ -138,6 +127,12 @@ function createSection(data, divID) {
 
 }
 
+function createDiv(id_name) {
+	var div = document.createElement("div");
+	div.setAttribute('id', id_name);
+	return div;
+}
+
 function createToggleImage(id_name, path) {
 
 	var image = $('<img />');
@@ -150,11 +145,10 @@ function createToggleImage(id_name, path) {
 	return image;
 }
 
-function createDiv(id_name) {
-	var div = document.createElement("div");
-	div.setAttribute('id', id_name);
-	return div;
+function capitalize(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
 
 function setImagePaddings() {
 
@@ -188,73 +182,3 @@ function setMainContentBounds() {
 	$(div).css('paddingLeft', "80px");
 }
 
-function capitalize(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-
-function toggleAll() {
-
-	if (globalToggle === 0) {
-		$(".toggle").css('opacity', '1');
-		globalToggle++;
-	} else {
-		$(".toggle").css('opacity', '0.4');
-		globalToggle--;
-	}
-
-
-}
-
-function apply_toggle() {
-	$( ".toggle" ).click(function() {
-
-		if( $(this).parent().attr("id") == "diaries") {
-			var path = $(this).attr("src");
-
-			var number = parseInt(path.slice(-5).charAt(0));
-
-			if($(this).css('opacity') != 1) {
-				$(this).css('opacity', '1');
-			}
-
-			else if(number === 1) {
-				$(this).attr("src", path.replace("1.png", "2.png"));
-				$(this).css('opacity', '1');
-			}
-			else if(number === 2) {
-				$(this).attr("src", path.replace("2.png", "3.png"));
-				$(this).css('opacity', '1');
-			}
-			else if(number === 3) {
-				$(this).attr("src", path.replace("3.png", "4.png"));
-				$(this).css('opacity', '1');
-			}
-			else if(number === 4) {
-				$(this).attr("src", path.replace("4.png", "1.png"));
-				$(this).css('opacity', '0.4');			
-			}
-
-			var src = $(this).attr("src");
-			var preloadedID = src.substring(15, src.length - 4);
-
-			var loadedImg = $("#" + preloadedID);
-
-			var w = parseInt($(loadedImg).css("width"));
-			var h = parseInt($(loadedImg).css("height"));
-
-			var newPadding =  ((40 - h) / 2) + "px " + ((40 - w) / 2) + "px";
-			$(this).css('padding', newPadding);
-
-		}
-
-		else {
-			var opacity = $( this ).css( "opacity" );
-			if (opacity === "1") {
-				$(this).css('opacity', '0.4');
-			} else {
-				$(this).css('opacity', '1');
-			}
-		}
-	});
-}
--- js/toggle.js
+++ js/toggle.js
@@ -1,4 +1,6 @@
-/* Eventlistener base file*/
+/* Toggle-based functions and helpers */
+
+var globalToggle = 0;
 
 var addEvent = function(object, type, callback) {
 	if (object == null || typeof(object) == 'undefined') return;
@@ -10,3 +12,69 @@ var addEvent = function(object, type, callback) {
 		object["on"+type] = callback;
 	}
 };
+
+function toggleAll() {
+
+	if (globalToggle === 0) {
+		$(".toggle").css('opacity', '1');
+		globalToggle++;
+	} else {
+		$(".toggle").css('opacity', '0.4');
+		globalToggle--;
+	}
+
+
+}
+
+function apply_toggle() {
+	$( ".toggle" ).click(function() {
+
+		if( $(this).parent().attr("id") == "diaries") {
+			var path = $(this).attr("src");
+
+			var number = parseInt(path.slice(-5).charAt(0));
+
+			if($(this).css('opacity') != 1) {
+				$(this).css('opacity', '1');
+			}
+
+			else if(number === 1) {
+				$(this).attr("src", path.replace("1.png", "2.png"));
+				$(this).css('opacity', '1');
+			}
+			else if(number === 2) {
+				$(this).attr("src", path.replace("2.png", "3.png"));
+				$(this).css('opacity', '1');
+			}
+			else if(number === 3) {
+				$(this).attr("src", path.replace("3.png", "4.png"));
+				$(this).css('opacity', '1');
+			}
+			else if(number === 4) {
+				$(this).attr("src", path.replace("4.png", "1.png"));
+				$(this).css('opacity', '0.4');			
+			}
+
+			var src = $(this).attr("src");
+			var preloadedID = src.substring(15, src.length - 4);
+
+			var loadedImg = $("#" + preloadedID);
+
+			var w = parseInt($(loadedImg).css("width"));
+			var h = parseInt($(loadedImg).css("height"));
+
+			var newPadding =  ((40 - h) / 2) + "px " + ((40 - w) / 2) + "px";
+			$(this).css('padding', newPadding);
+
+		}
+
+		else {
+			var opacity = $( this ).css( "opacity" );
+			if (opacity === "1") {
+				$(this).css('opacity', '0.4');
+			} else {
+				$(this).css('opacity', '1');
+			}
+		}
+	});
+}
